22e174a322a66390c06785fcab8dc4d479f425e2
Dahud Yusuf <abiodunyusuf4@gmail.com>
2022-11-12 20:43:11 +0100

    e-commerce product

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// ==================================
+// 1. Toggle humbugger menu;
+// By default, the .link tag should 
+// display none.
+// ==================================
+
+
+
+
+
+
+// ==================================
+// 2. Display products based on 
+// All, Men or Female categories.
+// ==================================
+
+
+
+
+
+
+// ==================================
+// 2. Display products based on 
+// search keywords in the input fields.
+// ==================================
