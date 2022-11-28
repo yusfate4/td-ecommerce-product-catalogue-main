44f7f6dcb63badf9b834d5b91190ca4bb5d75ba2
Dahud Yusuf <abiodunyusuf4@gmail.com>
2022-11-28 06:58:01 +0100

    Modified the JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,53 @@
 // ==================================
 // 1. Toggle humbugger menu;
-// By default, the .link tag should 
-// display none.
-// ==================================
-
-
-
-
-
 
 // ==================================
-// 2. Display products based on 
-// All, Men or Female categories.
-// ==================================
+
+const links = document.querySelector(".link");
+const hamburger = document.querySelector(".hamburger");
+const displayLink = () => {
+  links.style.display = "none";
+};
+displayLink();
+hamburger.addEventListener("click", () => {
+  hamburger.style.display = "block";
+  if (links.style.display === "block") {
+    links.style.display = "none";
+  } else {
+    links.style.display = "block";
+  }
+});
 
 
 
+// ======================================
 
 
+const search = document.querySelector(".search_product");
+search.addEventListener("input", () => {
+  const catalogue = document.querySelectorAll(".catalogue");
+  const check = document.querySelector(".check");
 
-// ==================================
-// 2. Display products based on 
-// search keywords in the input fields.
-// ==================================
+  catalogue.forEach((element) => {
+    element.style.display = "none";
+
+    product = search.value;
+    productEl = element.textContent.toUpperCase();
+    elProduct = element.textContent.toLowerCase();
+
+    if (
+      productEl.includes(product) ||
+      elProduct.includes(product) ||
+      element.textContent.includes(product)
+    ) {
+      element.style.display = "block";
+      check.textContent = " ";
+    } else {
+      element.style.display = "none";
+      check.textContent = "The Item Can't be Found!!!";
+      check.style.color = "white";
+      check.style.backgroundColor = "#EA047E";
+      check.style.fontSize = "40px";
+    }
+  });
+});
